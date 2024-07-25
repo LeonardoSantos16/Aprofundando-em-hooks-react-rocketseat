96855c2aaf3d52585505d96506494def07d0a19c
leonardo <leodsmf@gmail.com>
2024-07-25 13:45:41 -0300

    home e inputs commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" list="task-suggestions" placeholder="De um nome para o seu projeto"/>
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"/>
+            <option value="Projeto 2"/>
+            <option value="Projeto 3"/>
+            <option value="Banana"/>
+          </datalist>
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
